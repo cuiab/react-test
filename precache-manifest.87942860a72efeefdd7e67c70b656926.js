a0718ff48197163e5e3f5aed9b124e944c9f7597
cuiab <cab0720@163.com>
2019-07-12 17:01:55 +0800

    Updates

---
+++ precache-manifest.87942860a72efeefdd7e67c70b656926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2304257b2cb56ccf7876640b564c2a8d",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "662427cfe383d267ee79",
+    "url": "/react-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "662427cfe383d267ee79",
+    "url": "/react-test/static/js/main.39266df2.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
